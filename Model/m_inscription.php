a1c3804d6980397ddb4fe2387391c1805aae0b3c
DCHICHE <34024655+DCHICHE@users.noreply.github.com>
2017-12-01 14:12:57 +0100

    Change directory

---
+++ Model/m_inscription.php
@@ -0,0 +1,38 @@
+<?php
+
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=emballe_pese;charset=utf8', 'root', '');
+}
+catch (Exception $e)
+{	
+	die('Erreur : '. $e->getMessage());
+}
+
+$recov = $bdd->query('SELECT * FROM User');
+$exist = false;
+
+while ($donnees = $recov->fetch()){
+		if($mail == $donnees['Mail']){
+			$message = "User already taken";
+			$exist = true;
+		} 	
+}
+
+if($exist == false){
+
+$bdd->exec('INSERT INTO User VALUES (0,2,\''.$name.'\',\''.$forename.'\',\''.$adress.'\',\''.$cp.'\',\''.$city.'\',\''.$tel.'\',\''.$mail.'\',\''.$mdp.'\')');
+
+$message = "Vous êtes inscrit sur notre magnifique site de vrac ! Votre mdp est : ".$mdp." Ne les oubliez pas ! ;)
+<form action=\"../View/v_connexion.php\">
+<button>Retour à la page de connexion </button>";	
+
+}
+
+echo$message;
+	
+$recov->closeCursor();
+
+	
+
+?>
---
+++ View/v_connexion.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title>Connexion</title>
+	</head>
+	<body>
+<form action="../Controller/c_connexion.php" method="GET">
+    mail : <input type="text" name="mail"><p>
+    mdp : <input type="text" name="mdp"><p>
+	<button>Connexion</button>
+	</form>
+	</body>
+</html>

3f233df46db38fe219074f09f83a33a16811ee12
theo <emballepese@gmail.com>
2017-12-04 10:48:54 +0100

    css/connexion

--- View/v_connexion.php
+++ View/v_connexion.php
@@ -1,14 +1,37 @@
 <!DOCTYPE html>
-<html>
 
-	<head>
-		<title>Connexion</title>
-	</head>
-	<body>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Emballe Pese</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="../accueil/style.css" />
+</head>
+<body>
+<div id="wrap">
+  <div class="header">
+    <div id="menu">
+      <div class="logo">
+        <ul>
+          <li><a href="#">Statut : à venir</a></li>
+          <li class="selected"><a href="about.html">A propos</a></li>
+          <li><a href="about.html">Articles</a></li>
+          <li><a href="about.html">Mon panier</a></li>
+          <li><a href="about.html">Mon compte</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+<div class="p1">
+<table class="tab_center">
 <form action="../Controller/c_connexion.php" method="GET">
-    mail : <input type="text" name="mail"><p>
-    mdp : <input type="text" name="mdp"><p>
-	<button>Connexion</button>
+	<br><br><br>
+	<tr><td>Mail</td> <td>:</td> <td><input type="text" name="mail"></td></tr>
+	<tr><td>Mot de passe</td> <td>:</td> <td><input type="text" name="mdp"></td></tr>
+	<tr><td colspan=2></td><td style="text-align:center;"><button>Connexion</button></td></tr>
 	</form>
+</table>
+</div>
+</div>
 	</body>
 </html>

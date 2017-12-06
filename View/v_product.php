2c0815d86c754c4eca67642f33dc26df8e7c4130
Yassnif <yboucetta@intechinfo.fr>
2017-12-06 11:58:54 +0100

    redirectionz

---
+++ View/v_product.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Flower Shop - About</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="../Look/style.css" />
+</head>
+<body>
+<div id="wrap">
+  <div class="header">
+    <div id="menu">
+      <div class="logo">
+        <ul>
+          <li><a href="">Statut : à venir</a></li>
+          <li class="selected"><a href="../Index.html">Accueil</a></li>
+          <li><a href="v_product.php">Articles</a></li>
+          <li><a href="v_basket.php">Mon panier</a></li>
+          <li><a href="v_account.php">Mon compte</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class="center_content">
+    <h2>Voici les produits disponibles :</h2>
+  </div>
+</div>
+</body>
+</html>

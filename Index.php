c99c622cc35bab9562eaddae9b14b2f08d70587d
Unknown <dchiche94@hotmail.fr>
2017-12-08 09:48:07 +0100

    New Index

--- Index.php
+++ Index.php
@@ -1,8 +1,4 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<?php
-session_start();
-$user = $_SESSION['user'];
-?>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <title>Emballé-pesé</title>
@@ -19,7 +15,7 @@ $user = $_SESSION['user'];
           <li class="selected"><a href="Index.html">Accueil</a></li>
           <li><a href="View/v_product.php">Articles</a></li>
           <li><a href="View/v_basket.php">Mon panier</a></li>
-          <li><a href="View/v_account.php">Mon compte</a></li>
+          <li><a href="Controller/c_account.php">Mon compte</a></li>
         </ul>
       </div>
     </div>
@@ -30,10 +26,6 @@ $user = $_SESSION['user'];
       <div class="feat_prod_box_details">
 	  <p class="details"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud. <br />
 		  <br />
-	<?php
-		echo("Bonjour $user");
-		
-    ?>
       </div>
 </div>
 </body>

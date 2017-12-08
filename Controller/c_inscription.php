c31e845d568e6d8a51033e0125870660b4df3139
Unknown <dchiche94@hotmail.fr>
2017-12-08 11:57:15 +0100

    Inscription modif

--- Controller/c_inscription.php
+++ Controller/c_inscription.php
@@ -15,9 +15,9 @@ if(empty($_GET['mdp'])|| empty($_GET['name'])|| empty($_GET['forename'])|| empty
 		$correct_mail = false;
 	}
 	
-	if(strlen($tel)>10||strlen($cp)>5){
+	if(strlen($tel)!=10||strlen($cp)!=5){
 		include('../View/v_inscription.php');
-		echo"<h3 align='center'>Champ trop grand</h3>";
+		echo"<h3 align='center'>Champ trop grand/petit</h3>";
 	}else if(!is_numeric($cp)&&!empty($cp) || !is_numeric($tel)||strpos($mdp, ' ') !== false||strpos($name, ' ') !== false||strpos($forename, ' ') !== false||strpos($city, ' ') !== false||strpos($cp, ' ') !== false||strpos($tel, ' ') !== false||strpos($mail, ' ') !== false){
 		include('../View/v_inscription.php');
 		echo"<h3 align='center'>L'un des champs est incorrect</h3>";

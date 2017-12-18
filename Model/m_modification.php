160c0cd61979e52995a1444a829742ff88b92e28
Unknown <yboucetta@intechinfo.fr>
2017-12-18 14:50:48 +0100

    bdd

--- Model/m_modification.php
+++ Model/m_modification.php
@@ -2,7 +2,7 @@
 
     try
     {
-    	$bdd = new PDO('mysql:host=localhost;dbname=emballe_pese2;charset=utf8', 'root', 'MonMySQL');
+    	$bdd = new PDO('mysql:host=localhost;dbname=emballe_pese;charset=utf8', 'root', 'MonMySQL');
     }
     catch (Exception $e)
     {

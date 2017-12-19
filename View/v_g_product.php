bf5ac6f84d8bd4b2d88025cbbe4bf490842974e1
Unknown <yboucetta@intechinfo.fr>
2017-12-19 10:34:43 +0100

    html / inscription

--- View/v_g_product.php
+++ View/v_g_product.php
@@ -1,53 +1,50 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<title>Articles</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="stylesheet" type="text/css" href="../Look/style.css" />
-</head>
-<body>
-<div id="wrap">
-  <div class="header">
-    <div id="menu">
-      <div class="logo">
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Emballé-pesé</title>
+    <meta http-equiv="Content-Type" content="text/html" charset="utf-8" />
+    <link rel="stylesheet" type="text/css" href="../Look/style.css" />
+  </head>
+  <body>
+    <div id="main">
+      <div id="header">
+        <img id="logo" src="../Look/images/logo_accueil.png">
         <ul>
           <li><a href="">Statut : à venir</a></li>
-          <li class="selected"><a href="../Index_gerant.php">Accueil</a></li>
-		  <li><a href="../View/v_product.php">Articles</a></li>
+          <li><a href="../Index.php">Accueil</a></li>
+          <li><a href="../View/v_product.php">Articles</a></li>
           <li><a href="../Controller/c_basket.php">Mon panier</a></li>
           <li><a href="../Controller/c_account.php">Mon compte</a></li>
         </ul>
       </div>
-    </div>
-  </div>
-  <div class="center_content">
-	<h2>Voici les Articles en vente sur votre site :</h2></br>
-<table class="tab_center" cellpadding="5">
-<tr><th>Nom</th>
-<th>Quantité stock</th>
-<th>Compositions</th>
-<th>Date limite de consommation</th>
-<th>Marge</th>
-<th>Prix vente</th></tr>
+      <div id="content">
+        <h2>Voici les Articles en vente sur votre site :</h2></br>
+        <table class="tab_center" cellpadding="5">
+          <tr><th>Nom</th>
+            <th>Quantité stock</th>
+            <th>Compositions</th>
+            <th>Date limite de consommation</th>
+            <th>Marge</th>
+            <th>Prix vente</th></tr>
 
-<?php
-for($i=0;$i<count($tab);$i++){
-		echo"<tr><td>".$tab[$i]['Nom']."</td>";
-		echo"<td>".$tab[$i]['Qte_stock']."</td>";
-		echo"<td>".$tab[$i]['Composition']."</td>";
-		echo"<td>".$tab[$i]['DLC']."</td>";
-		echo"<td>".$tab[$i]['Marge']."</td>";
-		$prix = $tab[$i]['Prix_achat']*$tab[$i]['Marge'];
-		echo"<td>".$prix."€</td></tr>";
-	}
-	echo $message;
-?>
-</table>
-	</br>
-	 <form action="../Controller/c_modify_product.php">
-    <button> Modifier vos produits </button>
-    </form>
-  </div>
-</div>
-</body>
+            <?php
+            for($i=0;$i<count($tab);$i++){
+              echo"<tr><td>".$tab[$i]['Nom']."</td>";
+              echo"<td>".$tab[$i]['Qte_stock']."</td>";
+              echo"<td>".$tab[$i]['Composition']."</td>";
+              echo"<td>".$tab[$i]['DLC']."</td>";
+              echo"<td>".$tab[$i]['Marge']."</td>";
+              $prix = $tab[$i]['Prix_achat']*$tab[$i]['Marge'];
+              echo"<td>".$prix."€</td></tr>";
+            }
+            echo $message;
+            ?>
+          </table>
+        </br>
+        <form action="../Controller/c_modify_product.php">
+          <button> Modifier vos produits </button>
+        </form>
+      </div>
+    </div>
+  </body>
 </html>
--- View/v_inscription.php
+++ View/v_inscription.php
@@ -26,11 +26,11 @@
     			<tr><td>Nom*</td> <td>:</td> <td><input type=\"text\" name=\"name\" value=\"$name\"></td></tr><p>
     			<tr><td>Prenom*</td> <td>:</td> <td><input type=\"text\" name=\"forename\" value=\"$forename\"></td></tr><p>
     			<tr><td>Adresse</td> <td>:</td> <td><input type=\"text\" name=\"adress\" value=\"$adress\"></td></tr><p>
-    			<tr><td>Code Postal</td> <td>:</td> <td><input type=\"text\"name=\"cp\" maxlength=\"4\" value=\"$cp\"></td></tr><p>
+    			<tr><td>Code Postal</td> <td>:</td> <td><input type=\"text\"name=\"cp\" maxlength=\"5\" value=\"$cp\"></td></tr><p>
     			<tr><td>Ville</td> <td>:</td> <td><input type=\"text\" name=\"city\"value=\"$city\"></td></tr><p>
     			<tr><td>Mail*</td> <td>:</td> <td><input type=\"text\" name=\"mail\"value=\"$mail\"></td></tr><p>
     			<tr><td>Mot de passe*</td> <td>:</td> <td><input type=\"password\" name=\"mdp\"value=\"$mdp\"></td></tr><p>
-    			<tr><td>Tel*</td> <td>:</td> <td><input type=\"text\" name=\"tel\"value=\"$tel\"></td></tr><p>
+    			<tr><td>Tel*</td> <td>:</td> <td><input type=\"text\" name=\"tel\" maxlength=\"10\" value=\"$tel\"></td></tr><p>
           <tr><td>S'inscrire en tant que*</td> <td>:</td>
           <td colspan=3><input type=\"radio\" name=\"type\"value=\"1\"> Fermier </input>
           <input type=\"radio\" name=\"type\"value=\"2\"> Client </input>
